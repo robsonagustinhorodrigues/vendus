e8fd11a6a5d8faed7a00ff21d5701082a36a7968
Robson Agustinho Rodrigues <robsonagustinhorodrigues@gmail.com>
2025-06-21 14:05:34 -0300

    ajust port

--- app.py
+++ app.py
@@ -41,4 +41,6 @@ app.register_blueprint(api)
 
 # Executando a aplicação
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    host = os.environ.get("HOST", "0.0.0.0")
+    app.run(host=host, port=port, debug=True)
